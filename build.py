a7a0a99393b61898b710822278e13144e502448b
dale <dkfurrow@gmail.com>
2021-06-16 13:37:30 -0500

    Added command-line interface to build.py, small changes to README.md.

--- build.py
+++ build.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""
+Basic python build script to clean, build and package
+Google Message for Desktop
+Author: Dale Furrow
+License: MIT
+"""
 import json
 import logging
 import logging.handlers
@@ -6,10 +12,10 @@ import os
 import shutil
 import subprocess
 import sys
-import re
 from datetime import datetime
 from pathlib import Path
 from subprocess import Popen
+import argparse
 
 log_location = "./logs"
 log_filename = "messagesbuild.log"
@@ -25,6 +31,7 @@ output_folders = ['./Linux', './Mac', './Windows']
 build_command = ['/usr/bin/npm', 'run', 'build:all']
 zip_config = {'Linux': ['Linux', 'linux'], 'Mac': ['Mac', 'mac'], 'Windows': ['Windows', 'windows'],
               'Windows_Tray': ['Windows', 'windows-tray']}
+
 
 class MessageHandler:
     logger = None
@@ -64,7 +71,9 @@ class MessageHandler:
     def log(self, msg, lvl=logging.INFO):
         self.logger.log(lvl, msg)
 
+
 MESSAGEHANDLER = MessageHandler()
+
 
 def run_command(commands: list):
     MESSAGEHANDLER.log("running {0}".format(" ".join(commands)))
@@ -76,7 +85,6 @@ def run_command(commands: list):
     return p.returncode, std_output
 
 
-
 class BuildMessages:
     _version: str = None
 
@@ -85,6 +93,7 @@ class BuildMessages:
 
     @staticmethod
     def clean():
+        MESSAGEHANDLER.log("Cleaning distribution directory and zipfiles...")
         if Path(distribution_folder).exists():
             MESSAGEHANDLER.log("Removing {0}".format(str(Path(distribution_folder).absolute())))
             shutil.rmtree(Path(distribution_folder))
@@ -102,6 +111,7 @@ class BuildMessages:
         return version
 
     def package(self):
+        MESSAGEHANDLER.log('Zipping output to directories...')
         for k, v in zip_config.items():
             path_to_zip = Path(distribution_folder, k)
             full_out_path = Path(v[0], "google-messages-{0}_v{1}".format(v[1], self._version))
@@ -110,31 +120,36 @@ class BuildMessages:
 
     @staticmethod
     def build():
+        MESSAGEHANDLER.log('Building from Nativefier and Electron...')
         ret_code, output = run_command(build_command)
         MESSAGEHANDLER.log("Built with returncode {0}".format(str(ret_code)))
         MESSAGEHANDLER.log("Build output begins...")
         MESSAGEHANDLER.log(str(output))
         MESSAGEHANDLER.log("End Build output...")
 
-
-if __name__ == '__main__':
-
+def main(args):
+    MESSAGEHANDLER.log(args)
     MESSAGEHANDLER.log(os.getcwd())
-    nativefier_version = (run_command(["nativefier", "--version"]))[1]
-    MESSAGEHANDLER.log("printing nativefier version")
-    MESSAGEHANDLER.log(nativefier_version)
-
     build_messages = BuildMessages()
-    build_messages.clean()
-    build_messages.build()
-    build_messages.package()
-
+    if args.action == 'clean':
+        build_messages.clean()
+    elif args.action == 'build':
+        build_messages.build()
+    elif args.action == 'package':
+        build_messages.package()
+    elif args.action == 'all':
+        build_messages.clean()
+        build_messages.build()
+        build_messages.package()
+    else:
+        MESSAGEHANDLER.log("{0}: Unrecognized Command!".format(args))
     MESSAGEHANDLER.log("program finished")
 
 
-"""
 
- 
-dir = 'path/to/dir'
-shutil.rmtree(dir)
-"""
+if __name__ == '__main__':
+    build_parser = argparse.ArgumentParser('Clean, Build and/or Package Google Messages App')
+    build_parser.add_argument('action', metavar='action', nargs='?', type=str, help='accepts: clean, build, package, or all')
+    args = build_parser.parse_args()
+    main(args)
+
